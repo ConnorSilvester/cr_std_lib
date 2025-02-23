11030d08dbeb0c9588694baeacb6749396024982
Connor Silvester <connorsilvester23@gmail.com>
2025-02-23 14:04:21 +0000

    Added scripts folder, added build-linux.sh

---
+++ scripts/build-linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set project root directory
+PROJECT_ROOT=$(dirname "$(dirname "$(realpath "$0")")")
+BUILD_DIR="$PROJECT_ROOT/build"
+
+# Create build directory if it doesn't exist
+mkdir -p "$BUILD_DIR"
+
+cd "$BUILD_DIR" || exit
+cmake ..
+cmake --build .
