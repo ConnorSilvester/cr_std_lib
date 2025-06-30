15eb280caedaebee93b90b5731c1f332b353913e
ConnorSilvester <connorsilvester23@gmail.com>
2025-06-30 07:58:48 +0100

    Added more logging features, write to file track history

--- include/cr_std_logger.h
+++ include/cr_std_logger.h
@@ -11,6 +11,8 @@
 #define CR_STD_LOGGER_LOG_LEVEL_ERROR 2
 #define CR_STD_LOGGER_LOG_LEVEL_NONE 3
 
+typedef struct Vector Vector;
+
 /**
  * @brief Used to log to the console
  *
@@ -37,4 +39,43 @@ void cr_std_logger_outf(int log_type, const char *formatted_str, ...);
  * @return `1` if level is not valid and was changed to max allowed value.
  */
 int cr_std_logger_set_log_level(int log_level);
+
+/**
+ * @brief Used to set the flag to record logs.
+ *        Stores all logs in a Vector.
+ */
+void cr_std_logger_start_recording();
+
+/**
+ * @brief Used to disable the flag to record logs.
+ *        Does not store any new logs, keeps existing log history.
+ */
+void cr_std_logger_stop_recording();
+
+/**
+ * @brief Used to get the Vector of log history.
+ *
+ * @return `Vector` A Vector containing String * of the log messages.
+ */
+Vector *cr_std_logger_get_history();
+
+/**
+ * @brief Prints out all the logs stored in history.
+ */
+void cr_std_logger_print_history();
+
+/**
+ * @brief Empty the list of stored logs.
+ */
+void cr_std_logger_clear_history();
+
+/**
+ * @brief Writes the entire log history to a filepath.
+ *
+ * @param `filepath` the filepath for the output file.
+ *
+ * @return `0` on success.
+ * @return `1` on failure.
+ */
+int cr_std_logger_write_history_to_file(const char *filepath);
 #endif // CR_STD_LOGGER_H
--- src/cr_std_logger.c
+++ src/cr_std_logger.c
@@ -1,9 +1,16 @@
 #include "cr_std_logger.h"
+#include "cr_std_filesystem.h"
+#include "cr_std_string.h"
+#include "cr_std_vector.h"
 #include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 static int cr_std_logger_current_log_level = CR_STD_LOGGER_LOG_LEVEL_ALL;
+static bool cr_std_logger_record_log_flag = false;
+static Vector *cr_std_logger_log_history = NULL;
 
 void cr_std_logger_get_current_time(char *buffer, size_t buffer_size) {
     time_t raw_time;
@@ -16,7 +23,42 @@ void cr_std_logger_get_current_time(char *buffer, size_t buffer_size) {
     strftime(buffer, buffer_size, "%H:%M:%S", time_info);
 }
 
+void cr_std_logger_init_history() {
+    if (!cr_std_logger_log_history) {
+        cr_std_logger_log_history = cr_std_vector_new(String *);
+        cr_std_logger_log_history->free_function = cr_std_string_free_ptr;
+    }
+}
+
+void cr_std_logger_record_log(int log_type, const char *log_message) {
+    if (cr_std_logger_record_log_flag) {
+        cr_std_logger_init_history();
+
+        char time_str[9]; // HH:MM:SS + null terminator
+        cr_std_logger_get_current_time(time_str, sizeof(time_str));
+
+        String *log = NULL;
+        switch (log_type) {
+        case CR_STD_LOGGER_LOG_TYPE_INFO:
+            log = cr_std_string_newf("[%s] [INFO] %s\n", time_str, log_message);
+            break;
+        case CR_STD_LOGGER_LOG_TYPE_WARNING:
+            log = cr_std_string_newf("[%s] \033[33m[WARNING]\033[0m %s\n", time_str, log_message);
+            break;
+        case CR_STD_LOGGER_LOG_TYPE_ERROR:
+            log = cr_std_string_newf("[%s] \033[31m[ERROR]\033[0m %s\n", time_str, log_message);
+            break;
+        default:
+            break;
+        }
+        if (log) {
+            cr_std_vector_push_back(cr_std_logger_log_history, log);
+        }
+    }
+}
+
 void cr_std_logger_out(int log_type, const char *log_message) {
+    cr_std_logger_record_log(log_type, log_message);
     if (log_type < cr_std_logger_current_log_level) {
         return;
     }
@@ -39,10 +81,6 @@ void cr_std_logger_out(int log_type, const char *log_message) {
 }
 
 void cr_std_logger_outf(int log_type, const char *formatted_str, ...) {
-    if (log_type < cr_std_logger_current_log_level) {
-        return;
-    }
-
     va_list args;
     va_start(args, formatted_str);
 
@@ -75,4 +113,48 @@ int cr_std_logger_set_log_level(int log_level) {
     }
     cr_std_logger_current_log_level = log_level;
     return 0;
+}
+
+void cr_std_logger_start_recording() {
+    cr_std_logger_record_log_flag = true;
+}
+
+void cr_std_logger_stop_recording() {
+    cr_std_logger_record_log_flag = false;
+}
+
+Vector *cr_std_logger_get_history() {
+    return cr_std_logger_log_history;
+}
+
+void cr_std_logger_print_history() {
+    cr_std_logger_init_history();
+    for (int i = 0; i < cr_std_logger_log_history->size; i++) {
+        String *current_log = cr_std_vector_get_at(cr_std_logger_log_history, String, i);
+        printf("%s", current_log->c_str);
+    }
+}
+
+void cr_std_logger_clear_history() {
+    cr_std_vector_free(&cr_std_logger_log_history);
+    cr_std_logger_log_history = NULL;
+}
+
+int cr_std_logger_write_history_to_file(const char *filepath) {
+    cr_std_logger_init_history();
+    int result = 0;
+    if (cr_std_logger_log_history->size > 0) {
+        StringBuilder *sb = cr_std_string_builder_new("");
+        for (int i = 0; i < cr_std_logger_log_history->size; i++) {
+            String *current_log = cr_std_vector_get_at(cr_std_logger_log_history, String, i);
+            cr_std_string_builder_append_single(sb, current_log->c_str);
+        }
+        String *output = cr_std_string_builder_to_string(sb);
+        result = cr_std_filesystem_write_to_file(filepath, output->c_str);
+        cr_std_string_builder_free(&sb);
+        cr_std_string_free(&output);
+    } else {
+        result = cr_std_filesystem_write_to_file(filepath, "");
+    }
+    return result;
 }
